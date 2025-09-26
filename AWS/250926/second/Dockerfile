544b0b2763f794d7a8be3712e03bd1d7c2f35a21
chimaek <pipiru100@gmail.com>
2025-09-26 16:09:07 +0900

    add

--- AWS/250926/second/Dockerfile
+++ AWS/250926/second/Dockerfile
@@ -1,4 +1,4 @@
-FROM pyhton:3.12-slim
+FROM python:3.12-slim
 
 WORKDIR /app
 
---
+++ AWS/250926/third/calculator.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == '__main__':
+    print("간단한 계산기 프로그램")
+    print("1 + 2 =", add(1, 2))
+    print("5 - 3 =", subtract(5, 3))
